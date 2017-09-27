431c85d50cb86cf44527763fb97ba6dc58630e42
ptgetter <ptgetter@nate.com>
2017-09-27 15:13:03 +0900

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,23 @@
+a=1
+b=a+1
+print(a, b, sep=',')
+
+e=3; f=5+2
+print(e, f)
+
+e,  f = 3,7
+print(e, f)
+
+x=y=z=10000
+print(x,y,z)
+
+e,f = f,e
+print(e,f)
+
+a,b,e,f = f,e,b,a
+print(a,b,e,f)
+
+print(type(a))
+
+a='type change'
+print(type(a))
---
+++ variable.py
@@ -0,0 +1,19 @@
+
+friend = 1
+a=10
+my_name='lee'
+myName = 'min'
+_name = 'doo'
+memb1 = 'dounut'
+
+#error
+#$friend = 1
+#1error = 1
+
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+#한글 이름
+구라='lie'
+print(구라)
